535041cce3f8c5693e30ccd10c289e32d2efb4dd
Jeremy Evert on Morgan <jeremy.evert@swosu.edu>
2025-09-26 14:49:55 -0500

    added more fun stuff.

--- Perudo v.0.2/Perudo v.0.2/Reference Code/ResourceManager.cpp
+++
@@ -1,3 +0,0 @@
-#include "ResourceManager.h"
-
-std::map<std::string, std::unique_ptr<sf::Texture>> ResourceManager::textures;
--- Perudo v.0.2/Perudo v.0.2/Reference Code/ServerMain.cpp
+++
@@ -1,9 +0,0 @@
-#include "Server.h"
-#include <iostream>
-
-int main() {
-    std::cout << "Starting Perudo server on port 54000...\n";
-    Server server;
-    server.start(54000);
-    return 0;
-}
